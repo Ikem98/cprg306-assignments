c92e580509dafdaca533eab9687478013ca16ff2
Samuel Anowai <109921144+Ikem98@users.noreply.github.com>
2024-02-12 19:25:40 -0700

    Week 4

--- app/page.js
+++ app/page.js
@@ -6,7 +6,10 @@ export default function Page() {
         
         CPRG 306: Web Development 2 - Assignments</h1>
        
-        <div className="italic text-2xl flex justify-center"> <Link href="./week-2" > week-2 page</Link></div>  
+        <div className="italic text-2xl flex justify-center"> <Link href="./week-2" > week-2 page</Link></div> 
+        <div className="italic text-2xl flex justify-center"> <Link href="./week-3" > week-3 page</Link></div>  
+        <div className="italic text-2xl flex justify-center"> <Link href="./week-4" > week-4 page</Link></div>  
+        
       
          
     </main>
---
+++ app/week-4/page.js
@@ -0,0 +1,7 @@
+import NewItem from "./new-item";
+
+export default function week4Page () {
+    return (
+        <NewItem/>
+    )
+}
